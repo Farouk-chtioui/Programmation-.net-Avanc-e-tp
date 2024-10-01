d8f27e26c6140077d5df63679967e15812a86817
Farouk_chtioui <chtiouiaa25@gmail.com>
2024-10-01 16:25:18 +0100

    new tp

---
+++ EX3 tp1 c#/EX3 tp1 c#/Article.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EX3_tp1_c_
+{
+    internal class Article
+    {
+        private string refernce;
+        private string designation;
+        private double prixHt;
+        private static decimal tauxTva;
+
+        public string Refernce { get => refernce; set => refernce = value; }
+        public string Designation { get => designation; set => designation = value; }
+        public double PrixHt { get => prixHt; set => prixHt = value; }
+        public static decimal TauxTva
+        {
+            get { return tauxTva; }
+            set { tauxTva = value; }
+        }
+
+        public Article() { }
+        public Article(string refernce, string designation, double prixHt)
+        {
+            this.refernce = refernce;
+            this.designation = designation;
+            this.prixHt = prixHt;
+        }
+        public Article(string refernce, string designation)
+        {
+            this.refernce = refernce;
+            this.designation = designation;
+        }
+        public Article(Article existingArticle)
+        {
+            this.refernce = existingArticle.refernce;
+            this.designation = existingArticle.designation;
+            this.prixHt = existingArticle.prixHt;
+        }
+        public double CalculerPrixTTC()
+        {
+            return prixHt + (prixHt * (double)TauxTva / 100);
+        }
+        public void Afficher()
+        {
+            Console.WriteLine("Référence : " + refernce);
+            Console.WriteLine("Désignation : " + designation);
+            Console.WriteLine("Prix HT : " + prixHt);
+            Console.WriteLine("Le prix TTC est " + CalculerPrixTTC());
+        }
+    }
+}
---
+++ EX3 tp1 c#/EX3 tp1 c#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EX3_tp1_c_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Donner le taux de TVA pour tous les articles : ");
+            Article.TauxTva = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Le taux TVA est : " + Article.TauxTva + "%");
+
+            int articleCount = 1;
+            while (true)
+            {
+                Console.WriteLine($"Article {articleCount}:");
+
+                Console.Write("Donner la référence: ");
+                string refernce = Console.ReadLine();
+
+                Console.Write("Donner la désignation: ");
+                string designation = Console.ReadLine();
+
+                Console.Write("Donner le prix HT: ");
+                double prixHt = Convert.ToDouble(Console.ReadLine());
+
+                var article = new Article(refernce, designation, prixHt);
+                article.Afficher();
+
+                Console.Write("Voulez-vous ajouter un autre article? (o/n): ");
+                if (Console.ReadLine().ToLower() != "o")
+                {
+                    break;
+                }
+
+                articleCount++;
+            }
+        }
+    }
+}
