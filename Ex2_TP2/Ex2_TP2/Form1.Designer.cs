79036178fff028742e5a8e8ca56ad0a208dda643
Farouk_chtioui <chtiouiaa25@gmail.com>
2024-10-14 11:33:20 +0100

    added tp2 ext2

---
+++ Ex2_TP2/Ex2_TP2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ex2_TP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_text = new System.Windows.Forms.Button();
+            this.lbl_displayText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_text
+            // 
+            this.btn_text.Location = new System.Drawing.Point(282, 58);
+            this.btn_text.Name = "btn_text";
+            this.btn_text.Size = new System.Drawing.Size(177, 23);
+            this.btn_text.TabIndex = 0;
+            this.btn_text.Text = "Show";
+            this.btn_text.UseVisualStyleBackColor = true;
+            this.btn_text.Click += new System.EventHandler(this.btn_text_Click);
+            // 
+            // lbl_displayText
+            // 
+            this.lbl_displayText.AutoSize = true;
+            this.lbl_displayText.Location = new System.Drawing.Point(341, 135);
+            this.lbl_displayText.Name = "lbl_displayText";
+            this.lbl_displayText.Size = new System.Drawing.Size(0, 13);
+            this.lbl_displayText.TabIndex = 1;
+            this.lbl_displayText.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_displayText);
+            this.Controls.Add(this.btn_text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_text;
+        private System.Windows.Forms.Label lbl_displayText;
+    }
+}
+
---
+++ Ex2_TP2/Ex2_TP2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex2_TP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_text_Click(object sender, EventArgs e)
+        {
+            if (lbl_displayText.Visible)
+            {
+                lbl_displayText.Visible = false;
+                btn_text.Text = "Show";
+            }
+            else
+            {
+                lbl_displayText.Text = "The button was clicked!";
+                lbl_displayText.Visible = true;
+                btn_text.Text = "Don't show";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
