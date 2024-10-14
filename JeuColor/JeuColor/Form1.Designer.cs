2f266b341c0546e19ca099a524369cb7fd038738
Farouk_chtioui <chtiouiaa25@gmail.com>
2024-10-14 11:23:11 +0100

    added ex3

---
+++ JeuColor/JeuColor/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace JeuColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_default = new System.Windows.Forms.Button();
+            this.btn_green = new System.Windows.Forms.Button();
+            this.btn_blue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_default
+            // 
+            this.btn_default.Enabled = false;
+            this.btn_default.Location = new System.Drawing.Point(270, 79);
+            this.btn_default.Name = "btn_default";
+            this.btn_default.Size = new System.Drawing.Size(158, 23);
+            this.btn_default.TabIndex = 0;
+            this.btn_default.Text = "Default color";
+            this.btn_default.UseVisualStyleBackColor = true;
+            this.btn_default.Click += new System.EventHandler(this.btn_default_Click);
+            // 
+            // btn_green
+            // 
+            this.btn_green.Location = new System.Drawing.Point(270, 126);
+            this.btn_green.Name = "btn_green";
+            this.btn_green.Size = new System.Drawing.Size(158, 23);
+            this.btn_green.TabIndex = 1;
+            this.btn_green.Text = "Green";
+            this.btn_green.UseVisualStyleBackColor = true;
+            this.btn_green.Click += new System.EventHandler(this.btn_green_Click);
+            // 
+            // btn_blue
+            // 
+            this.btn_blue.Location = new System.Drawing.Point(270, 181);
+            this.btn_blue.Name = "btn_blue";
+            this.btn_blue.Size = new System.Drawing.Size(158, 23);
+            this.btn_blue.TabIndex = 2;
+            this.btn_blue.Text = "Blue";
+            this.btn_blue.UseVisualStyleBackColor = true;
+            this.btn_blue.Click += new System.EventHandler(this.btn_blue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_blue);
+            this.Controls.Add(this.btn_green);
+            this.Controls.Add(this.btn_default);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_default;
+        private System.Windows.Forms.Button btn_green;
+        private System.Windows.Forms.Button btn_blue;
+    }
+}
+
---
+++ JeuColor/JeuColor/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeuColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btn_blue_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+            EnableAllButtons();
+            btn_blue.Enabled = false;
+        }
+
+        private void btn_green_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+            EnableAllButtons();
+            btn_green.Enabled = false;
+        }
+
+        private void btn_default_Click(object sender, EventArgs e)
+        {
+            this.BackColor = DefaultBackColor;
+            EnableAllButtons();
+            btn_default.Enabled = false;
+        }
+
+        private void EnableAllButtons()
+        {
+            btn_blue.Enabled = true;
+            btn_green.Enabled = true;
+            btn_default.Enabled = true;
+        }
+    }
+}
